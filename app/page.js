720312f9200e83294baabad173fd861ef0c8d48b
Shivam Dubey <96882359+Dubeyshivam502@users.noreply.github.com>
2025-04-12 17:02:51 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import {Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>welcome back to my applications</h2>
+      <Button>About me </Button>
+    </div>
+  );
+}
